c709a3238bc041ff2cf65276c9a4efa8ebcfa181
Sindhu Wijaya Mulyo Nugroho, S.Kom <88322797+sindhu-nugroho@users.noreply.github.com>
2026-05-12 21:23:38 +0700

    Add PokemonResponse and PokemonResult data classes

--- pokemart/pokemon.kt
+++ pokemart/pokemon.kt
@@ -1,1 +1,20 @@
+package com.example.e_media.model
+
+data class PokemonResponse(
+    val results: List<PokemonResult>
+)
+
+data class PokemonResult(
+    val name: String,
+    val url: String
+) {
+    val id: Int
+        get() = url.trimEnd('/').split('/').last().toInt()
+    
+    val imageUrl: String
+        get() = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$id.png"
+
+    val price: Double
+        get() = (id * 10 + 5.99)
+}
 
